ad524b16022da7c87344f16fadac66d6a067e255
bymbaamedehgui-beep <bymbaamedehgui@gmail.com>
2026-03-16 00:42:28 +0800

    feat: PWA support - installable app

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,26 @@
+import Head from 'next/head';
+import { useEffect } from 'react';
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(() => {});
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="theme-color" content="#dc2626" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="EYESH Checker" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <title>EYESH Checker</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
